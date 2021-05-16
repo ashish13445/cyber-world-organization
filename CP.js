2cbc5dfc30afbbf806ca6753b6670b5c83f8fdf5
ashish13445 <65503198+ashish13445@users.noreply.github.com>
2021-05-17 02:21:32 +0530

    Add files via upload

---
+++ CP.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+function CP() {
+    return(
+        <>
+
+<div className="container  min-vh-100 cp">
+        <div className="row">
+          
+          <div  data-aos ='fade-up' className="col">
+            <h1><b>Our Community Partners</b>
+            </h1>
+            <br />
+            <br />
+            <p>Our community has single dominating active body "CYBER WORLD ORG" which consists of: 4 Several Subdivisions</p>
+            <ul>
+              <li>NOOB HACK3RS COMMUNITY, NHC- Hack3rs Community</li>
+              <li>Cyber Suraksha- Information & Media.</li>
+              <li>Cyber Salvager-Digital Forensics.</li>
+              <li>Cyber Crime Agency- Cyber Crime Investigation</li>
+            </ul>
+            <br />
+            <h3><b>Partners</b> </h3>
+            <p> BeFojji- Operational Security Squad</p>
+            <p> Anti Cyber Crime Society (Anti-Phishing Wing)</p>
+            <p>Gurugram Cyber Cell</p>
+            <p>Bsides Maharashtra</p>
+            <p>CySec Alliance</p>
+            <p>Crime Free Bharat</p>
+          </div>
+          <div className="col">
+            <img src="cyber.jpg" alt="partners" className='container cyber'/>
+
+          </div>
+        </div>
+
+
+
+
+      </div>
+        
+        </>
+    )
+}
+
+export default CP;
